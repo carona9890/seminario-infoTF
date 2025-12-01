c73f5a58e1b8499ba0d1f4778bd38e279c31b596
G9890 <jerocarona5@gmail.com>
2025-12-01 02:42:26 -0300

    final2

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = function(req, res, next) {
 
   try {
     const decoded = jwt.verify(token.replace("Bearer ", ""), process.env.JWT_SECRET);
-    req.user = decoded; // { id: ... }
+    req.user = decoded; 
     next();
   } catch (err) {
     res.status(401).json({ error: "Token inválido" });
